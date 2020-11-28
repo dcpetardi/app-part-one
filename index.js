71fd4cc9b2b4a7704a5717aa80d2ad111cd4f773
dcpetardi <dc.petardi@gmail.com>
2020-11-28 11:17:20 -0500

    another message

--- index.js
+++ index.js
@@ -404,7 +404,7 @@ app.post("/message", (req, res) => {
 	}else if(!parsedBody.hasOwnProperty('channelName'))  {	
 		res.send(JSON.stringify({"success":false,"reason":"channelName field missing"}))
 		return
-    }else if(!(channelUsers.get(channelName).includes(username))){
+    }else if(!channelUsers.get(channelName).includes(username)){
 		res.send(JSON.stringify({"success":false,"reason":"User is not part of this channel"}))
 		return
 	}else if(!channel.has(channelName)){
